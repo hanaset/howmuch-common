a417fe65e1221a8d04bad1e1c19e44f3b4473d1d
Luke <b183523@gmail.com>
2020-03-11 13:56:54 +0900

    EsConfig Common으로 이동

---
+++ src/main/java/com/how/muchcommon/config/EsConfig.java
@@ -0,0 +1,33 @@
+package com.how.muchcommon.config;
+
+import lombok.Getter;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Getter
+@Configuration
+@PropertySource("classpath:properties/elasticsearch/elasticsearch-${spring.profiles.active}.properties")
+@EnableElasticsearchRepositories(basePackages = "com.how.muchcommon.repository.elasticrepository")
+public class EsConfig {
+
+    @Value("${elasticsearch.host}")
+    private String esHost;
+
+    @Value("${elasticsearch.index}")
+    private String esIndex;
+
+    @Value("${elasticsearch.type}")
+    private String type;
+
+    @Bean
+    RestHighLevelClient restHighLevelClient() {
+        return new RestHighLevelClient(RestClient.builder(HttpHost.create(esHost)));
+
+    }
+}
