4997e0d00ad59df68dfd31da8e794740a14d8383
Luke <b183523@gmail.com>
2020-03-16 00:51:56 +0900

    수정

--- src/main/java/com/how/muchcommon/repository/jparepository/CookieRepository.java
+++ src/main/java/com/how/muchcommon/repository/jparepository/CookieRepository.java
@@ -2,7 +2,9 @@ package com.how.muchcommon.repository.jparepository;
 
 import com.how.muchcommon.entity.jpaentity.CookieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CookieRepository extends JpaRepository<CookieEntity, String> {
     CookieEntity findBySite(String site);
 }
