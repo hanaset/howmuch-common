5906f2e4580413ac789c74666afab64a4896d84d
Mark <dhkdn4042@naver.com>
2020-03-20 01:13:23 +0900

    MarketName enum 추가 및 es entity 수정

---
+++ src/main/java/com/how/muchcommon/model/type/MarketName.java
@@ -0,0 +1,17 @@
+package com.how.muchcommon.model.type;
+
+import lombok.Getter;
+
+@Getter
+public enum MarketName {
+    joonggonara("joonggonara"),
+    danggn("danggn"),
+    bunzang("bunzang")
+    ;
+
+    private String name;
+
+    MarketName(String name) {
+        this.name = name;
+    }
+}
